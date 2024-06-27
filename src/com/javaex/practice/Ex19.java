f6362307977b42595bb8c5071b5db6acb867a06a
aripos1 <ihsjjang@gmail.com>
2024-06-27 17:34:58 +0900

    수정

--- src/com/javaex/practice/Ex19.java
+++ src/com/javaex/practice/Ex19.java
@@ -13,41 +13,31 @@ public class Ex19 {
 		
 		while(true) {
 		
-		System.out.println("----------------------------");
-		System.out.println("1.예금 | 2.출금 | 3.잔고| 4.종료");
-		System.out.println("----------------------------");
-		System.out.print("선택>");
-		int num = sc.nextInt(); 
+			System.out.println("----------------------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고| 4.종료");
+			System.out.println("----------------------------");
+			System.out.print("선택>");
+			int num = sc.nextInt(); 
 			
-		if(num==4) {
-			System.out.println("프로그램 종료");
-			break;
-		}else if(num==1) {
-			
-			System.out.print("예금액>");
-			int dep = sc.nextInt();
-			sum = sum +dep;
-			
-		}else if(num==2) {
-		
-			System.out.print("출금액>");
-			int wid = sc.nextInt();
-			sum = sum -wid;
-			
-			
-		}else if(num==3) {
-					 
-			System.out.println("잔액>"+sum);
-			
-		}else {
-			System.out.println("다시 입력해주세요.");
+			if(num==4) {
+				System.out.println("프로그램 종료");
+				break;
+			}else if(num==1) {
+				System.out.print("예금액>");
+				int dep = sc.nextInt();
+				sum = sum +dep;
+			}else if(num==2) {
+				System.out.print("출금액>");
+				int wid = sc.nextInt();
+				sum = sum -wid;
+			}else if(num==3) {
+				System.out.println("잔액>"+sum);
+			}else {
+				System.out.println("다시 입력해주세요.");
+			}
 		}
-			
-		
-		}
-		
-		}
-		
+		sc.close();
+	}
 }
 		
 
--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -51,7 +51,7 @@ public class Ex20 {
 //			}
 			
 	}
-		
+		sc.close();
 	}
 }
 }
