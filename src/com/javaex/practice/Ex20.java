f6b21fb73e7cd149cbb7904270d9e7bd06d33732
aripos1 <ihsjjang@gmail.com>
2024-06-28 15:43:40 +0900

    2024-06-28

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -3,55 +3,45 @@ package com.javaex.practice;
 import java.util.Scanner;
 
 public class Ex20 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
+		int s = (int) (Math.random() * 10) + 1;
+		System.out.println(s);
 		
-		System.out.println("======================");
-		System.out.println("   [숫자맞추기게임 시작]    ");
-		System.out.println("======================");
-		
-		
-		
-		while(true) {
-		System.out.print(">>");
-		int num =sc.nextInt();
-		if (num>29) {
-			System.out.println("더 낮게");
-		}else if(num<29) {
-			System.out.println("더 높게");
-		}else {
-			System.out.println("맞았습니다.");
-			System.out.print("게임을 종료하시겠습니까?(y/n) >>");
-			String str = sc.next();
+		while (true) {
+			System.out.println("======================");
+			System.out.println("   [숫자맞추기게임 시작]    ");
+			System.out.println("======================");
+			System.out.print(">>");
+			int num = sc.nextInt();
+
 			
-			if (str .equals(str="y")) {
-				System.out.println("======================");
-				System.out.println("   [숫자맞추기게임 종료]    ");
-				System.out.println("======================");
-				break;
-			}else if (str .equals(str="n")) {
-				System.out.println("======================");
-				System.out.println("   [숫자맞추기게임 시작]    ");
-				System.out.println("======================");
+			if (num > s) {
+				System.out.println("더 낮게");
+			} else if (num < s) {
+				System.out.println("더 높게");
+			} else {
+				System.out.println("맞았습니다.");
+
+				System.out.print("게임을 종료하시겠습니까?(y/n) >>");
+
+				String str = sc.next();
+					if (str.equals("y")) {
+						System.out.println("======================");
+						System.out.println("   [숫자맞추기게임 종료]    ");
+						System.out.println("======================");
+						break;
+
+					} else if (str.equals("n")) {
+
+					} else {
+						System.out.println("잘못입력했습니다.다시 입력하세요.");
+					}
+
 			}
-			
-			
-//			switch(str) {
-//				case "y":
-//				System.out.println("======================");
-//				System.out.println("   [숫자맞추기게임 종료]    ");
-//				System.out.println("======================");
-//				break;
-//				default :
-//				System.out.println("======================");
-//				System.out.println("   [숫자맞추기게임 시작]    ");
-//				System.out.println("======================");
-//			}
-			
+
+		}
 	}
-		sc.close();
-	}
-}
 }
